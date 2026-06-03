6cd151aae2a52abb38d26263739b8f1b0358784c
Mac Mealey <148462018+Maccy-0@users.noreply.github.com>
2026-06-02 20:02:45 -0400

    Added the object scripts

---
+++ Eye-Scry/Assets/ObjectMovementScript.cs
@@ -0,0 +1,162 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ObjectMovementScript : MonoBehaviour
+{
+
+    public float currentWedgeSize;
+
+    public bool isGrabbable = false;
+    public bool isSelected = false;
+    private Vector2 origPivot = Vector2.zero;
+    private Vector2 targetPivot = Vector2.zero;
+    public Rigidbody2D thisRB;
+    public float forceEmit = 5f;
+    public float dampen = 1f;
+
+    public bool isSold = false;
+
+    // Item types, used in association with a specific slot.
+    [Tooltip("Swiss = 1 // Brie = 2 // Weight1 = 50 // Weight2 = 100 // weight3 = 250 // weight4 = 500")]
+    // Swiss = 1 // Brie = 2 // Weight1 = 50 // Weight2 = 100 // weight3 = 250 // weight4 = 500
+    public int objectType;
+    //
+    public bool submitToTaker = false;
+    public bool beingSlotted = false;
+    public SlotScript controlTaken;
+
+
+    // -----
+    void Start()
+    {
+        origPivot = transform.localPosition;
+        if(this.GetComponent<Rigidbody2D>() != null)
+        {
+            thisRB = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // -----
+    void Update()
+    {
+        if (isSelected)
+        {
+
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           // Vector3 offset = new Vector3(targetPivot.x, targetPivot.y, 0);
+            Vector3 truePos = new Vector3(thisRB.position.x,thisRB.position.y,0);
+            Vector2 direction = (mousePosition - truePos);
+            Vector2 dampener = -thisRB.linearVelocity * dampen;
+
+
+            thisRB.AddForce((direction * forceEmit)+ dampener, ForceMode2D.Force);
+        }
+        else if (controlTaken != null && !isSelected && beingSlotted)
+        {
+            Debug.Log("5 Alert");
+            Vector2 targetPos = controlTaken.gameObject.transform.position;
+            
+            Vector2 dampener = -thisRB.linearVelocity * dampen;
+            Vector2 currentPos = new Vector2(this.transform.position.x, this.transform.position.y);
+            Vector2 threshHold = new Vector2(.01f, .01f);
+            Vector2 threshHoldTwo = new Vector2(.3f,.3f);
+
+            thisRB.AddForce(((targetPos - currentPos) * forceEmit) + dampener, ForceMode2D.Force);
+
+            if (targetPos.x - currentPos.x < threshHold.x && targetPos.x - currentPos.x > -threshHold.x && targetPos.y - currentPos.y < threshHold.y && targetPos.y - currentPos.y > -threshHold.y)
+            {
+                targetPos = currentPos;
+                isGrabbable = true;
+                thisRB.constraints = RigidbodyConstraints2D.FreezeAll;
+                beingSlotted = false;
+            }
+            else if (targetPos.x - currentPos.x < threshHoldTwo.x && targetPos.x - currentPos.x > -threshHoldTwo.x && targetPos.y - currentPos.y < threshHoldTwo.y && targetPos.y - currentPos.y > -threshHoldTwo.y)
+            {
+                thisRB.MovePosition(targetPos);
+            }
+
+
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        if (isGrabbable)
+        {
+            takeHold();
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (beingSlotted == true)
+        {
+            Debug.Log("LUP1 Alert");
+            isSelected = false;
+            submitToTaker = true;
+            targetPivot = origPivot;
+        }
+
+        else if (isSelected)
+                {
+            Debug.Log("LUP3 Alert");
+            isSelected = false;
+            targetPivot = origPivot;
+                }
+    }
+
+    void OnMouseEnter()
+    {
+        if(!isSelected)
+        {
+//            Debug.Log("Mouse is here!");
+            isGrabbable = true;
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+        if (!isSelected)
+        {
+//            Debug.Log("Mouse is no longer here!");
+            isGrabbable = false;
+        }
+    }
+
+    void takeHold()
+    {
+        isSelected = true;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+       // Vector3 worldPosition = this.transform.InverseTransformPoint(mousePosition);
+        targetPivot.x = mousePosition.x;
+        targetPivot.y = mousePosition.y;
+
+        thisRB.constraints = RigidbodyConstraints2D.None;
+
+        if (controlTaken != null)
+        {
+            controlTaken.objectLeft(this.gameObject);
+            controlTaken = null;
+        }
+
+        submitToTaker = false;
+        beingSlotted = false;
+    }
+
+    public void GetSlotted(SlotScript slot)
+    {
+        Debug.Log("9 Alert");
+        isSelected = false;
+        controlTaken = slot;
+    }
+
+    public void SelfDestruct()
+    {
+        DestroyImmediate(this.gameObject);
+    }
+
+}
---
+++ Eye-Scry/Assets/SlotScript.cs
@@ -0,0 +1,114 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SlotScript : MonoBehaviour
+{
+    // When making a slot for a specific purpose I recomend creating a new script as parent to the slot, or group of slots. This system doesn't need to interact with the hierarchy otherwise.
+
+    [Tooltip("Object Types")]
+    // If a slot needs a specific item, you can set it here via its associated integer
+    public int objectType;
+
+    // Whether any type of object is allowed in. Set to true if any object can enter the slot.
+    public bool ignoreType = false;
+
+    public int selfIndex;
+
+    public ObjectMovementScript heldItem = null;
+    public ObjectMovementScript wantsToBeHeld = null;
+    
+    // If a specific slot is more than just a holder use this integer to index.
+    public int keyPointType;
+
+    public CircleCollider2D selfCollider;
+    private List<GameObject> detectedObjects = new List<GameObject>();
+
+    void Start()
+    {
+        if (this.gameObject.GetComponent<CircleCollider2D>() != null)
+        {
+            selfCollider = this.gameObject.GetComponent<CircleCollider2D>();
+        }
+        if(selfCollider != null)
+        {
+            detectedObjects = new List<GameObject>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(wantsToBeHeld != null && heldItem == null && wantsToBeHeld.submitToTaker == true)
+        {
+            Debug.Log("4 Alert");
+            heldItem = wantsToBeHeld;
+            wantsToBeHeld.GetSlotted(this);
+            wantsToBeHeld.submitToTaker = false;
+        }
+    }
+
+    public void  OnTriggerStay2D(Collider2D collision)
+    {
+            // Gets the items in range and determines if it's ready to be slotted
+            if (heldItem == null && collision.gameObject.CompareTag("Object"))
+            {
+                Debug.Log("1 Alert");
+                if (!detectedObjects.Contains(collision.gameObject))
+                {
+                    Debug.Log("2 Alert");
+                    detectedObjects.Add(collision.gameObject);
+
+                    if (collision.gameObject.GetComponent<ObjectMovementScript>() && wantsToBeHeld == null)
+                    {
+                        if (collision.gameObject.GetComponent<ObjectMovementScript>().objectType == this.objectType || this.ignoreType)
+                        {
+                            Debug.Log("3 Alert");
+                            wantsToBeHeld = collision.gameObject.GetComponent<ObjectMovementScript>();
+                            wantsToBeHeld.beingSlotted = true;
+                        }
+
+                    }
+
+                }
+            }    
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (wantsToBeHeld != null)
+        {
+
+            if (wantsToBeHeld.GetComponent<ObjectMovementScript>() && collision.gameObject == wantsToBeHeld.gameObject)
+            {
+                objectLeft(wantsToBeHeld.gameObject);
+
+            }
+        }
+    }
+
+    // Logic for item leaving the slot
+    public void objectLeft(GameObject lostObject)
+    {
+        if (wantsToBeHeld != null)
+        {
+            if (detectedObjects.Contains(wantsToBeHeld.gameObject))
+            {
+                detectedObjects.Remove(lostObject);
+                Debug.Log("Object left slot: " + selfIndex);
+            }
+
+            wantsToBeHeld.beingSlotted = false;
+            wantsToBeHeld.submitToTaker = false;
+            heldItem = null;
+            wantsToBeHeld = null;
+        }
+    }
+
+    public void ResetSlot()
+    {
+        this.heldItem = null;
+        this.wantsToBeHeld = null;
+        this.detectedObjects = new List<GameObject>();
+    }
+}
