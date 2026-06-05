48523aa7c97e743817cafefd05dc619ee05b23d5
Mac Mealey <148462018+Maccy-0@users.noreply.github.com>
2026-06-05 18:32:51 -0300

    Finished the puzzle logic

--- Eye-Scry/Assets/Scripts/PuzzleScenesLogic.cs
+++ Eye-Scry/Assets/Scripts/PuzzleScenesLogic.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Unity.XR.OpenVR;
 using UnityEngine;
 
@@ -26,6 +27,21 @@ public class PuzzleScenesLogic : MonoBehaviour
     public GameObject Hat;
     public GameObject Rack;
     public ObjectMovementScript HatScript;
+
+    public GameObject fakeRing;
+    public GameObject realRing;
+    public GameObject ringContainer;
+    public GameObject gem1obj;
+    public GameObject gem2obj;
+    public GameObject gem3obj;
+    public GameObject gem4obj;
+    public GameObject gem5obj;
+    public ObjectMovementScript gem1;
+    public ObjectMovementScript gem2;
+    public ObjectMovementScript gem3;
+    public ObjectMovementScript gem4;
+    public ObjectMovementScript gem5;
+    public int[] gems = { 0, 0, 0, 0, 0 };
 
     public string currentMessage;
 
@@ -111,6 +127,24 @@ public class PuzzleScenesLogic : MonoBehaviour
                 Hat.transform.position = new Vector2(6.75f, 1.75f);
             }
         }
+        if (RingProgression == 0)
+        {
+            fakeRing.SetActive(true);
+            realRing.SetActive(false);
+            ringContainer.SetActive(true);
+        }
+        if (RingProgression == 1)
+        {
+            fakeRing.SetActive(false);
+            realRing.SetActive(true);
+            ringContainer.SetActive(false);
+        }
+        if (RingProgression == 2)
+        {
+            fakeRing.SetActive(false);
+            realRing.SetActive(false);
+            ringContainer.SetActive(false);
+        }
     }
 
     public void LoadTowerTop()
@@ -153,6 +187,51 @@ public class PuzzleScenesLogic : MonoBehaviour
         {
             WandProgression = 3;
             Debug.Log("Wand got");
+        }
+        if (message == "1GemSlot" && RingProgression == 0)
+        {
+            gems[0] = 1;
+            gem1.thisRB.simulated = false;
+            Destroy(gem1);
+            gem1obj.transform.position = new Vector2(-6f, 0.5f);
+        }
+        if (message == "2GemSlot" && RingProgression == 0)
+        {
+            gems[1] = 1;
+            gem2.thisRB.simulated = false;
+            Destroy(gem2);
+            gem2obj.transform.position = new Vector2(-5.5f, 0.5f);
+        }
+        if (message == "3GemSlot" && RingProgression == 0)
+        {
+            gems[2] = 1;
+            gem3.thisRB.simulated = false;
+            Destroy(gem3);
+            gem3obj.transform.position = new Vector2(-5f, 0.5f);
+        }
+        if (message == "4GemSlot" && RingProgression == 0)
+        {
+            gems[3] = 1;
+            gem4.thisRB.simulated = false;
+            Destroy(gem4);
+            gem4obj.transform.position = new Vector2(-4.5f, 0.5f);
+        }
+        if (message == "5GemSlot" && RingProgression == 0)
+        {
+            gems[4] = 1;
+            gem5.thisRB.simulated = false;
+            Destroy(gem5);
+            gem5obj.transform.position = new Vector2(-4f, 0.5f);
+        }
+        if (gems.Sum() == 5 && RingProgression == 0)
+        {
+            RingProgression = 1;
+            Debug.Log("did it");
+        }
+        if (message == "Key2" && RingProgression == 1)
+        {
+            RingProgression = 2;
+            Debug.Log("Ring got");
         }
     }
 }
