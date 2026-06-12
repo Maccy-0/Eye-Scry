066c5ff444566fdd34bb4cd43c50ff6f09721edf
Remaek <brooksmt@sheridancollege.ca>
2026-06-12 09:40:51 -0400

    Onboarding Scene

---
+++ Eye-Scry/Assets/Scripts/AcessabilityQuestionCard.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+
+public class AccessibilityQuestionCard : MonoBehaviour
+{
+    public enum AccessibilityType
+    {
+        ColorSensitivity,
+        DyslexiaFonts,
+        AuditoryAssistance
+    }
+
+    [Header("Settings")]
+    public AccessibilityType questionType;
+
+    [Header("Manager Reference")]
+    [Tooltip("Drag your 'Manager' GameObject from the hierarchy here.")]
+    public AccessibilityManager manager;
+
+    [Header("Color Sensitivity Dependencies")]
+    public GameObject colorblindVolume;
+
+    [Header("Dyslexia Fonts Dependencies")]
+    public TMP_FontAsset dyslexiaFont;
+    private TMP_FontAsset defaultFont;
+
+    [Header("Auditory Dependencies")]
+    public AudioMixer audioMixer;
+    public string dialogueVolumeParameter = "DialogueVolume";
+
+    // This is the direct function your UI buttons will click!
+    public void SelectOption(bool isYes)
+    {
+        Debug.Log($"[Card] Chosen option: {(isYes ? "Yes" : "No")} on {gameObject.name}");
+
+        switch (questionType)
+        {
+            case AccessibilityType.ColorSensitivity:
+                HandleColorSensitivity(isYes);
+                break;
+            case AccessibilityType.DyslexiaFonts:
+                HandleDyslexiaFonts(isYes);
+                break;
+            case AccessibilityType.AuditoryAssistance:
+                HandleAuditoryAssistance(isYes);
+                break;
+        }
+
+        if (manager != null)
+        {
+            manager.AdvanceToNextCard();
+        }
+        else
+        {
+            Debug.LogError($"Manager is missing on {gameObject.name}! Can't switch cards.");
+        }
+    }
+
+    private void HandleColorSensitivity(bool enabled)
+    {
+        if (colorblindVolume != null) colorblindVolume.SetActive(enabled);
+    }
+
+    private void HandleDyslexiaFonts(bool enabled)
+    {
+        TextMeshProUGUI[] allTextElements = FindObjectsOfType<TextMeshProUGUI>(true);
+        foreach (TextMeshProUGUI textElement in allTextElements)
+        {
+            if (enabled)
+            {
+                if (defaultFont == null) defaultFont = textElement.font;
+                if (dyslexiaFont != null) textElement.font = dyslexiaFont;
+            }
+            else
+            {
+                if (defaultFont != null) textElement.font = defaultFont;
+            }
+        }
+    }
+
+    private void HandleAuditoryAssistance(bool enabled)
+    {
+        if (audioMixer != null)
+        {
+            float targetVolume = enabled ? 10f : 0f;
+            audioMixer.SetFloat(dialogueVolumeParameter, targetVolume);
+        }
+    }
+}
